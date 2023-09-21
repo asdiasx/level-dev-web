dc4a72e26e71ab618345c9c8c0cf4ea8fd3ec4e4
Adriano Dias <adriano.dias@locaweb.com.br>
2023-09-21 14:34:02 -0300

    refactoring

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,11 @@
 class TopicsController < ApplicationController
-before_action :set_topic, only: %i[ show edit update destroy ]
+  before_action :set_topic, only: %i[show edit update destroy]
 
   def index
     @topics = Topic.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @topic = Topic.new
@@ -16,20 +15,19 @@ before_action :set_topic, only: %i[ show edit update destroy ]
     topic_params = params.require(:topic).permit(:titulo)
     @topic = Topic.new(topic_params)
     if @topic.save
-      redirect_to topic_url(@topic), notice: "Tópico criado"
+      redirect_to topic_url(@topic), notice: 'Tópico criado'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     topic_params = params.require(:topic).permit(:titulo)
 
     if @topic.update(topic_params)
-      redirect_to topic_url(@topic), notice: "Tópico atualizado"
+      redirect_to topic_url(@topic), notice: 'Tópico atualizado'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -38,7 +36,7 @@ before_action :set_topic, only: %i[ show edit update destroy ]
   def destroy
     @topic.destroy
 
-    redirect_to topics_url, notice: "Tópico removido"
+    redirect_to topics_url, notice: 'Tópico removido'
   end
 
   private
